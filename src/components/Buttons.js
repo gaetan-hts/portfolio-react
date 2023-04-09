d9fa1ad7787bc16727f06d95ebc197165707f353
GRE3DISGOOD <gaetanhonthaas@gmail.com>
2023-04-09 20:25:41 +0200

    update responsive

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,22 +6,22 @@ const Buttons = (props) => {
     <div className="scroll-bottom">
       {props.left && (
         <NavLink to={props.left} className="left hover">
-          <span>⮜</span>
+          <span>&#10092;</span>
         </NavLink>
       )}
       {props.right && (
         <NavLink to={props.right} className="right hover">
-          <span>⮞</span>
+          <span>&#10093;</span>
         </NavLink>
       )}
       {props.bot && (
         <NavLink to={props.bot} className="bot hover">
-          <span>⮟</span>
+          <span>&#10093;</span>
         </NavLink>
       )}
       {props.top && (
         <NavLink to={props.top} className="top hover">
-          <span>⮝</span>
+          <span>&#10093;</span>
         </NavLink>
       )}
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,35 +12,12 @@ const Navigation = () => {
           <li>accueil</li>
         </NavLink>
 
-        <li className="nav-portfolio">
-          portfolio
-          <ul className="nav-projects">
-            <NavLink
-              to="/projet-1"
-              className={(nav) => (nav.isActive ? "nav-active hover" : "hover")}
-            >
-              <li>projet 1</li>
-            </NavLink>
-            <NavLink
-              to="/projet-2"
-              className={(nav) => (nav.isActive ? "nav-active hover" : "hover")}
-            >
-              <li>projet 2</li>
-            </NavLink>
-            <NavLink
-              to="/projet-3"
-              className={(nav) => (nav.isActive ? "nav-active hover" : "hover")}
-            >
-              <li>projet 3</li>
-            </NavLink>
-            <NavLink
-              to="/projet-4"
-              className={(nav) => (nav.isActive ? "nav-active hover" : "hover")}
-            >
-              <li>projet 4</li>
-            </NavLink>
-          </ul>
-        </li>
+        <NavLink
+          to="/projet-1"
+          className={(nav) => (nav.isActive ? "nav-active hover" : "hover")}
+        >
+          <li>portfolio</li>
+        </NavLink>
 
         <NavLink
           to="/contact"
