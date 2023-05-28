07637d2ac6a629b7abe9a59063f862ec50c2c74c
GRE3DISGOOD <gaetanhonthaas@gmail.com>
2023-05-28 18:05:48 +0200

    add new project + new logo

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,7 +3,7 @@ export const projectsData = [
     id: 1,
     title: "Quai Antique",
     date: "Janvier 2023",
-    languages: ["React", "Php", "Sass"],
+    languages: ["React", "Php", "Sass", "SQL"],
     infos:
       "Projet fictif de restaurant gastronomique français proposant une carte, une galerie d'images, un formulaire de réservation et un système d'authentification pour conserver les données utilisateur mais aussi d'accéder à la partie administrateur pour modifier le site (CRUD de la galerie d'images, des horaires et du nombre de couverts max par service).",
     img: "./assets/img/projet-1.png",
@@ -12,8 +12,8 @@ export const projectsData = [
   {
     id: 2,
     title: "Crypto Dashboard",
-    date: "April 2023",
-    languages: ["React", "(Redux Toolkit)"],
+    date: "Avril 2023",
+    languages: ["React", "Sass", "(Redux Toolkit)"],
     infos:
       "Dashboard de crypto-monnaies pour suivre en temps réel les prix et les variations de vos crypto-monnaies préférées. Grâce à l'utilisation de la bibliothèque React, la gestion de l'état avec Redux Toolkit et l'API de CoinGecko, cette application fournit une expérience utilisateur rapide et fluide.",
     img: "./assets/img/crypto-dashboard.png",
@@ -21,22 +21,22 @@ export const projectsData = [
   },
   {
     id: 3,
+    title: "Cindy Joseph",
+    date: "Mai 2023",
+    languages: ["React", "Sass", "(Redux Toolkit)"],
+    infos:
+      "Site vitrine One Page d'une danseuse chorégraphe, mettant en avant son talent artistique à travers un design épuré. Ce site propose une version anglaise gérée avec Redux Toolkit, un carrousel d'images dynamique réalisé avec React-Responsive-Carousel, ou encore un formulaire de contact utilisant EmailJS. La navigation intuitive et minimaliste offre une expérience utilisateur efficace.",
+    img: "./assets/img/cindy-joseph.png",
+    link: "https://cindy-joseph.fr/",
+  },
+  {
+    id: 4,
     title: "Crèche Gros Calin",
     date: "Août 2022",
     languages: ["Wordpress"],
     infos:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+      "Site WordPress One Page pour une crèche associative utilisant les fonctionnalités d'Elementor et le thème OceanWP. Pour optimiser les performances du site, j'ai utilisé des outils tels que Autoptimize, WP Fastest Cache et Async JS. On y retrouve des sections tel qu'une galerie d'images, un formulaire de contact interactif ou encore un carrousel de témoignages. La navigation offre une expérience utilisateur fluide et engageante pour les visiteurs.",
     img: "./assets/img/projet-3.png",
     link: "http://creche-groscalin.fr/",
-  },
-  {
-    id: 4,
-    title: "Creative Dev",
-    date: "Juillet 2020",
-    languages: ["Vue", "Php"],
-    infos:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-    img: "./assets/img/projet-4.jpg",
-    link: "http://www.google.com",
   },
 ];
